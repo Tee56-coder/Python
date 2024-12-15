3584065b579a5bcc72d7b9867b28c6c77c193bd0
Tee56 <sultanbalogun32@gmail.com>
2024-12-15 21:58:06 +0100

    message

---
+++ index.py
@@ -0,0 +1,44 @@
+print('hello world!')
+# print is a funnction in python 
+
+# variables
+
+age = 20
+price = 19.95
+first_name = 'solomon'
+# print(age)
+
+# boolean
+is_online = True
+
+# exercise
+patients_name = 'john smith'
+patients_age = 20
+patients_status = 'new' 
+
+# name = input('what is your name? ')
+# print('hello ' + name)
+
+# type conversion
+int()
+float()
+bool()
+str()
+
+birth_year = input('enter your birth year: ')
+age = 2024 - int(birth_year)
+print(age)
+
+
+# exercise 2
+first_num = 90
+second_num = 60.1
+Sum = input(first_num + second_num)
+print(Sum)
+
+
+# strings
+course = 'pyhton for beginners'
+
+# .find()
+# to find a given value or string in the str
